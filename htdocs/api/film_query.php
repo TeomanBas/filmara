76db5f81a7abb769d928ad3a1d591ec5a209168d
Teoman BAŞ <teomanbas@protonmail.com>
2023-07-23 00:50:37 +0300

    Api http istek fonksiyonu ve bu fonksiyon için cevap fonksiyonu yazıldı.

---
+++ htdocs/api/film_query.php
@@ -0,0 +1,21 @@
+<?php 
+function film_query($film){
+    $title=$film->tip;
+    require_once("./config/config.php");
+    $domain = DOMAIN;
+    $api_key = API_KEY;
+    $parameters = "api_url_parameters";
+    if($title=="t"){
+        $url=$domain."?apikey=".$api_key."&t=".$film->film.$parameters;
+    }else{
+        $url=$domain."?apikey=".$api_key."&s=".$film->film;
+    }
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+    $response = curl_exec($ch);
+    curl_close($ch);
+    unset($domain,$api_key,$url,$data,$film,$title);
+    print_r($response);
+}
+?>
