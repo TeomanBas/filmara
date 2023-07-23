4b5b4cf935162eb66920508bb394b566a0c89465
Teoman BAŞ <teomanbas@protonmail.com>
2023-07-23 05:13:18 +0300

    database bağlantısı için config dosyasına gerekli erişim bilgileri tanımlandı

--- htdocs/api/config/config.php
+++ htdocs/api/config/config.php
@@ -1,4 +1,12 @@
 <?php 
+// Requests
 define("API_KEY","api_key");
 define("DOMAIN","api_domain");
+// Database
+define("HOSTNAME","database_server_ip");
+define("USER","database_user");
+define("PASSWORD","database_password");
+define("DATABASE","database_name");
+define("PORT","database_server_port");
+define("TABLE","visitor_table");
 ?>
